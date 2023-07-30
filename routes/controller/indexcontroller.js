22daad83e93070233f9d4396e2a653aad0d21a36
kpndevroot <vishnukpn884@gmail.com>
2023-07-30 20:02:59 +0530

    msg change

--- routes/controller/indexcontroller.js
+++ routes/controller/indexcontroller.js
@@ -1,6 +1,6 @@
 export const indexControll = async (req, res, next) => {
   try {
-    res.status(200).json({ status: true, msg: "HI DEV" });
+    res.status(200).json({ status: true, msg: "HI DEV API ONLINE" });
   } catch (err) {}
 };
 export const testController = async (req, res, next) => {
